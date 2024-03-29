a2bf4124ca3dfc08a5ec465ce899c9dc288d6127
Alex DeMeo <alex.demeo129@gmail.com>
2024-03-28 19:30:45 -0700

    fix cicd (#4)
    * add col
    
    * test
    
    * allow-gable-pre-release

--- pyspark/job.py
+++ pyspark/job.py
@@ -1,20 +1,11 @@
-import argparse
+from script import run_job
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
-from script import run_job
-
-def parse_arguments(argv=None):
-    ap = argparse.ArgumentParser()
-    ap.add_argument("--final_output_table")
-    return ap.parse_args(argv)
-
-
 if __name__ == "__main__":
-    # Parse args
-    args_main = parse_arguments()
-    final_output_table = args_main.final_output_table
+    final_output_table = "pnw_bookings_30_days"
     print(f"final_output_table: {final_output_table}")
     
     spark = SparkSession.builder.getOrCreate()
